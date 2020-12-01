41cf0d426a8ad4c8a7b49359087d30e90e225973
Lucas <lucasvalenteds@gmail.com>
2020-12-01 18:30:55 -0300

    Moving clone logic to a function

--- script.sh
+++ script.sh
@@ -5,10 +5,15 @@ repositories_directory="repositories"
 
 [ ! -d "$repositories_directory" ] && mkdir "$repositories_directory"
 
-awk --field-separator=" " '{print $1" "$2" "$3}' "$repositories" \
-    | while read -r hostname username repository
-do
-    url="https://$hostname/$username/$repository"
-    directory="$repositories_directory/${username}__${repository}"
-    git clone "$url" "$directory"
-done
+clone() {
+    awk --field-separator=" " '{print $1" "$2" "$3}' "$repositories" \
+        | while read -r hostname username repository
+        do
+            url="https://$hostname/$username/$repository"
+            directory="$repositories_directory/${username}__${repository}"
+            git clone "$url" "$directory"
+
+        done
+}
+
+clone
